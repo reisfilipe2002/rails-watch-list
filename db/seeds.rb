aed9c0b723e8a9d702788ca604983dfdb2669b0b
filipe reis <reisfilipe102@gmail.com>
2023-05-25 17:09:59 -0300

    Created a WatchList

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.open(url).read
+films = JSON.parse(user_serialized)
+
+films_array = films['results']
+
+films_array.each do |film|
+  movie = Movie.new(title: film['title'], overview: film['overview'], poster_url: "https://image.tmdb.org/t/p/w500#{film['poster_path']}", rating: film['vote_average'])
+  movie.save
+end
